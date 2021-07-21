d177c3ec65951e6bf161819ec27249604aeb0975
Jesus Medina <chuymedina96@gmail.com>
2021-07-21 11:25:17 -0500

    first commit

---
+++ JavaAssignments/JavaBasicsOne/JavaBasicsOne.java
@@ -0,0 +1,70 @@
+// package JavaAssignments; // Getting error for package for some weird reason. Will investigate later
+/**
+ * @author chuymedina9613 
+ */
+public class JavaBasicsOne {
+    // First Pattern: 
+	static void printRows(int rows){
+        // loop handles looping through the number of rows given by user(or hardcoded in variable in this case)
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<=i; j++){
+                // print stars FOR as long as j is less than or equal to i
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    //Second Pattern:
+    static void printRowsDownUp(int rows){
+        for(int i=0; i<rows; i++){
+            for(int j=rows-1; j>i; j--){
+                // print stars FOR as long as j is less than or equal to i
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    //Third Pattern:
+    static void printPyramid(int rows){
+        for (int i=0; i<rows; i++) {
+            for (int j=rows-i; j>1; j--) {
+                System.out.print(" ");
+            }
+            for (int j=0; j<=i; j++ ) {
+                // printing stars
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    //Fouth Pattern:
+    static void printPyramidUpDown(int rows){
+        //Printing the pattern
+        for (int i = 1; i <= rows; i++){
+          for (int j = 1; j < i; j++){
+                System.out.print(" ");
+            }
+          for (int j = i; j <= rows; j++){
+                System.out.print("* ");
+            }  
+            System.out.println();
+        }
+    }
+     
+
+    // Main method(entry point to any java program.)
+	public static void main(String args[])
+	{
+		int rows = 4;
+        // 1)
+		printRows(rows);
+        // 2)
+        printRowsDownUp(rows);
+        //3)
+        printPyramid(rows);
+        // 4)
+        printPyramidUpDown(rows);
+
+	}
+}
---
+++ JavaAssignments/JavaBasicsOne/JavaBasicsOne2.java
@@ -0,0 +1,65 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class JavaBasicsOne2 {
+	public static void main(String[] args)
+	{
+		int answer, guess;
+        // using final primitve data type
+		final int max = 100;
+
+		// Scanner for getting user input
+		Scanner in = new Scanner(System.in);
+
+		// Creating a new random instance
+		Random rand = new Random();
+
+		boolean correct = false;
+
+		// correct answer
+		answer = rand.nextInt(max) + 1;
+
+		// loop until the guess is correct
+		while (!correct) {
+
+			System.out.println(
+				"Guess a number between 1 and 100: ");
+
+			// guess value
+			guess = in.nextInt();
+
+			// if guess is greater than actual
+			if (guess > answer) {
+				System.out.println("Too high, try again");
+			}
+
+			// if guess is less than actual
+			else if (guess < answer) {
+				System.out.println("Too low, try again");
+			}
+            else if(guess + 10 > answer){
+                System.out.println("You within ten of the correct answer");
+                System.out.println(answer);
+            }
+            else if(guess -10 > answer){
+                System.out.println("You within ten of the correct answer");
+                System.out.println(answer);
+            }
+            else if(guess + 10 < answer){
+                System.out.println("You within ten of the correct answer");
+                System.out.println(answer);
+            }
+            else if(guess -10 < answer){
+                System.out.println("You within ten of the correct answer");
+                System.out.println(answer);
+            }
+			else {
+				System.out.println(
+					"Yes, you guessed the number.");
+
+				correct = true;
+			}
+		}
+		System.exit(0);
+	}
+}
