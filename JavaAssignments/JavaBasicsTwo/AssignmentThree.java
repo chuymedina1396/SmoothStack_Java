acc62101ab906ff3ef6e1573aaf541a6a30a25d6
Jesus Medina <chuymedina96@gmail.com>
2021-07-21 13:43:58 -0500

    tried problem 3. pretty tough. still researching

--- JavaAssignments/JavaBasicsTwo/AssignmentThree.java
+++ JavaAssignments/JavaBasicsTwo/AssignmentThree.java
@@ -1,3 +1,33 @@
+// I was pretty lost on this one. Still researching Shape interfaces.
+
 public interface Shape{
+    
+}
+
+public class ShapeBase implements Shape {
+
+    protected String name;
+    protected int width, height;
+
+    public ShapeBase(String name, int width, int height ){
+        super();
+        this.name = name;
+        this.width = width;
+        this.height = height;
+    }
+}
+
+public class Rectangle extends ShapeBase {
+
+}
+
+public class Circle extends ShapBase {
+    
+}
+
+public class Triangle extends ShapBase {
+    public Triangle(String name, int width, int height ){
+        super(name, width, height);
+    }
     
 }
