7bb954d04cfdb00c674398fb735222df6c504e33
Jesus Medina <chuymedina96@gmail.com>
2021-07-21 12:12:58 -0500

    first assignment

--- JavaAssignments/JavaBasicsTwo/AssignmentOne.java
+++ JavaAssignments/JavaBasicsTwo/AssignmentOne.java
@@ -1,12 +1,21 @@
+import java.util.Scanner;
 public class AssignmentOne {
-
-    static void addInput(){
-    
-       
-    }
     public static void main(String args[])
 	{
-		System.out.println("Assignment One");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int a = scan.nextInt();
+
+        System.out.println("Enter another number:");
+        int b = scan.nextInt();
+
+        System.out.println("Enter another number:");
+        int c = scan.nextInt();
+
+        System.out.println("Enter one more number:");
+        int d = scan.nextInt();
+
+        System.out.println("And the result is ..." + " " + (a + b + c + d));
 
 	}
 }
