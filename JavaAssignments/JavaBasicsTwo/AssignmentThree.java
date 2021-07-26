05539d95b24181a0893e7652c029c44ea0fda490
Jesus Medina <chuymedina96@gmail.com>
2021-07-26 13:09:58 -0500

    more

--- JavaAssignments/JavaBasicsTwo/AssignmentThree.java
+++ JavaAssignments/JavaBasicsTwo/AssignmentThree.java
@@ -4,8 +4,6 @@
 
 // Create a Shape Interface with the methods "calculateArea" and "display". Create a Rectangle, Circle, and Triangle class to implement that interface. 
 
-    // Your interface needs to declare the two methods specified in the requirements which is calculateArea and display. Then your classes that implement that interface should be overriding those methods. The calculateArea method should be calculating the area of each shape. The display method should be displaying the area that was calculated
-
 interface Shape{
     void calculateArea();
     void display();
