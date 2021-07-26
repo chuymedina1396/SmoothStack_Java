88951ea580c6e004c455ee5456c1e3856f8f11f8
Jesus Medina <chuymedina96@gmail.com>
2021-07-26 15:31:07 -0500

    more

--- JavaAssignments/JavaBasicsFive/JavaAssignments5/JavaWeekendAssignmentOne/JavaWeekEndAssignmentOne/src/WeekendAssignmentOne.java
+++ JavaAssignments/JavaBasicsFive/JavaAssignments5/JavaWeekendAssignmentOne/JavaWeekEndAssignmentOne/src/WeekendAssignmentOne.java
@@ -3,48 +3,13 @@
     // PerformOperation isOdd(): The lambda expression must return  if a number is odd or  if it is even.
     // PerformOperation isPrime(): The lambda expression must return  if a number is prime or  if it is composite.
     // PerformOperation isPalindrome(): The lambda expression must return  if a number is a palindrome or  if it is not.
-    // Input Format
-
-    // Input is handled for you by the locked stub code in your editor.
-
-    // Output Format
-
-    // The locked stub code in your editor will print  lines of output.
-
-    // Sample Input
-
-    // The first line contains an integer,  (the number of test cases).
-
-    // The  subsequent lines each describe a test case in the form of  space-separated integers:
-    // The first integer specifies the condition to check for ( for Odd/Even,  for Prime, or  for Palindrome). The second integer denotes the number to be checked.
-
-    // 5
-    // 1 4
-    // 2 5
-    // 3 898
-    // 1 3
-    // 2 12
-
-    // Sample Output
-
-    // EVEN
-    // PRIME
-    // PALINDROME
-    // ODD
-    // COMPOSITE
-
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.stream.*;
-import java.util.List;
-
 
 public class WeekEndAssignmentOne {
     public static void main(String[] args){
         Integer i = 4;
         isOdd(i);
         primeOrComposite(i);
-        isPalendrone();
+        isPalindrome(5);
 
     }
     public static boolean isOdd(int num) {
@@ -81,7 +46,6 @@ public class WeekEndAssignmentOne {
        int palindrome = num;
        int reverse = 0;
 
-        // Compute the reverse
         while (palindrome != 0) {
             int remainder = palindrome % 10;
             reverse = reverse * 10 + remainder;
