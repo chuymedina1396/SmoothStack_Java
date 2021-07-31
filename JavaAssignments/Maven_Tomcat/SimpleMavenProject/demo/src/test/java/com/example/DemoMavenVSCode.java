452129abe156fd809dbf5bee705de9e2a9a9ade8
Jesus Medina <chuymedina96@gmail.com>
2021-07-31 16:33:57 -0500

    more

--- JavaAssignments/Maven_Tomcat/SimpleMavenProject/demo/src/test/java/com/example/DemoMavenVSCode.java
+++ JavaAssignments/Maven_Tomcat/SimpleMavenProject/demo/src/test/java/com/example/DemoMavenVSCode.java
@@ -1,19 +1,19 @@
 package example;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class DemoMavenVSCode {
+// public class DemoMavenVSCode {
 
-    public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://demo.automationtesting.in");
-        Thread.sleep(3000);
-        driver.quit();
-    }
-    public void sampleMethod(){
-        System.out.println("This is a sample method");
-    }
-}
+//     public static void main(String[] args) {
+//         WebDriverManager.chromedriver().setup();
+//         WebDriver driver = new ChromeDriver();
+//         driver.get("http://demo.automationtesting.in");
+//         Thread.sleep(3000);
+//         driver.quit();
+//     }
+//     public void sampleMethod(){
+//         System.out.println("This is a sample method");
+//     }
+// }
